330e56680147d777b85b8b59ab1c0b415d817bc1
David Sánchez Valderrama <deivit_vma@hotmail.com>
2019-06-14 18:35:40 -0500

    Added the base case.

--- HanoiTowersAssembly/HanoiTowers.asm
+++ HanoiTowersAssembly/HanoiTowers.asm
@@ -5,7 +5,7 @@
 .text
 
 #Store in $s0 the number of disks
-addi $s0, $zero, 8
+addi $s0, $zero, 1
 #Store in $s1 the high portion of the initial address
 addi $s1, $zero, 0x1001
 #Shift left 16 bits for the low portion of the initial address
@@ -33,26 +33,39 @@ j fill_start_rod
 
 #Main function
 main: 
-#Store biggest disk in $a0
-addi $a0, $zero, 1
+#Store number of disks in $a0
+add $a0, $zero, $s0
 #Store reference for start rod in $a1
 add $a1, $zero, $s1
 #Store reference for end rod in $a2
 add $a2, $zero, $s3
 #Store reference for aux rod in $a3
 add $a3, $zero, $s2
+#Set $s7 to 1 for comparison for the base case(n==1)
+addi $s7, $zero, 1
 #Call Hanoi function
 jal hanoi_function
 #End
 j end
 
+#Recursive implementation of the Hanoi Towers algorithm
+#public static void hanoi_function(int n, int start, int end, int aux)
 hanoi_function:
+#Check if the disk is equal to 1, go to the base_case
+beq $a0, $s7, base_case
+
+
+
+#Base case when the number of disks is equal to 1
+base_case:
+#Substract 4 from the stack pointer of $a1 to get the value
+addi $a1, $a1, -4
+#Load the value from the start rod
+lw $v0, 0($a1)
+#Save the value into the end rod
+sw $v0, 0($a2)
+#Increment the stack pointer of $a2 for the next value to be stored
+addi $a2, $a2, 4
+
 
 end:
-
-
-
-
-
-
-

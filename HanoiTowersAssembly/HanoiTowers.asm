86c237f642e20c6fedfb691647ddd229802c4b0c
David Sánchez Valderrama <deivit_vma@hotmail.com>
2019-06-14 16:29:59 -0500

    Defined registers for the addresses of each rod, and the register for the number of disks.

--- HanoiTowersAssembly/HanoiTowers.asm
+++ HanoiTowersAssembly/HanoiTowers.asm
@@ -4,4 +4,19 @@
 .data
 .text
 
+#Store in $s0 the number of disks
+addi $s0, $zero, 3
+#Store in $s1 the high portion of the initial address
+addi $s1, $zero, 0x1001
+#Shift left 16 bits for the low portion of the initial address
+#Start rod address
+sll $s1, $s1, 16
+#Aux rod address 
+addi $s2, $s1, 0x0020
+#End rod address
+addi $s3, $s1, 0x0040
+
+
+
+
 
